da4668a59da6ff1f8d4e8a443b4b5cf51e45125b
DJ200148 <dillonjackson01@gmail.com>
2023-04-30 01:28:47 +0000

    Dillons parts finished.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -24,7 +24,16 @@ struct queue {
 // Dillon O(1)
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t queue = malloc(sizeof(struct queue));
+
+	// Check if malloc failed
+	if(queue == NULL)
+		return NULL;
+	// Else, initialize queue
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->length = 0;
+	return queue;
 }
 
 // Hayden O(1)
@@ -36,14 +45,47 @@ int queue_destroy(queue_t queue)
 // Dillon O(1)
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+	// Check for valid inputs
+	if (queue == NULL || data == NULL)
+		return -1;
+	
+	// Create new node
+	struct node * newNode = malloc(sizeof(struct node));
+	if (newNode == NULL)
+		return -1;
+	
+	// Initialize new node
+	newNode->data = data;
+	newNode->next = NULL;
+	
+	// Add new node to queue
+	if(queue->head == NULL)
+		queue->head = newNode;
+	queue->tail = newNode;
+	queue->length++;
+
+	return 0;
 }
 
 // Dillon O(1)
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+	// Check for valid inputs
+	if (queue == NULL || data == NULL || queue->head == NULL || queue->tail == NULL || queue->length == 0)
+		return -1;
+	
+	// Set head data from queue to data
+	*data = queue->head->data;
+
+	// Remove head from queue and update the queue
+	struct node * temp = queue->head;
+	queue->head = queue->head->next;
+	queue->length--;
+	free(temp);
+	
+	return 0;
 }
+
 // Hayden NOT O(1)
 int queue_delete(queue_t queue, void *data)
 {
