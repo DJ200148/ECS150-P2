7930ec3bb8d9a5a79f49ba52239550659e6c5948
DJ200148 <dillonjackson01@gmail.com>
2023-04-30 01:31:37 +0000

    Tail nulled if list empties

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -82,6 +82,9 @@ int queue_dequeue(queue_t queue, void **data)
 	queue->head = queue->head->next;
 	queue->length--;
 	free(temp);
+
+	if (queue->length == 0)
+		queue->tail = NULL;
 	
 	return 0;
 }
