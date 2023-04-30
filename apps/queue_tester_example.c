1832f360623b46977382c20317fb933e2f20625e
DJ200148 <dillonjackson01@gmail.com>
2023-04-30 03:30:25 +0000

    Co-authored-by: Hayden Balsys <haydenbalsys@users.noreply.github.com>

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -37,10 +37,136 @@ void test_queue_simple(void)
 	TEST_ASSERT(ptr == &data);
 }
 
+//test case to check if a new queue is empty
+void test_queue_empty(void) {
+	//make sure queue is empty
+	queue_t queue;
+	if (queue_is_empty(&queue) == 0) {
+		TEST_ASSERT(queue_is_empty(&queue) == 0 && "queue is empty");
+	}
+	fprintf(stderr, "*** TEST queue_simple ***\n");
+}
+
+void test_add_item(void)
+{
+	queue_t queue;
+    int ret = queue_init(&queue);
+    TEST_ASSERT(ret == 0 && "queue_init failed");
+}
+
 int main(void)
 {
 	test_create();
 	test_queue_simple();
+	test_queue_empty();
+	test_add_item();
 
 	return 0;
 }
+
+// Create a new queue using the queue_create() function.
+// Check if the queue is empty using the queue_empty() function.
+// The expected result is that the queue should be empty.
+// Test case to check if an item can be added to the queue
+
+
+// Create a new queue using the queue_create() function.
+// Add an item to the queue using the queue_enqueue() function.
+// Check if the queue is empty using the queue_empty() function.
+// The expected result is that the queue should not be empty.
+// Test case to check if multiple items can be added to the queue
+// int main() {
+//     queue_t queue;
+//     int data = 42;
+//     int ret = queue_init(&queue);
+//     assert(ret == 0 && "queue_init failed");
+
+//     ret = queue_enqueue(&queue, &data);
+//     assert(ret == 0 && "queue_enqueue failed");
+
+//     return 0;
+// }
+
+
+// Create a new queue using the queue_create() function.
+// Add multiple items to the queue using the queue_enqueue() function.
+// Check the size of the queue using the queue_size() function.
+// The expected result is that the size of the queue should match the number of items added.
+// Test case to check if an item can be removed from the queue
+// int main() {
+//     queue_t queue;
+//     int data = 42;
+//     int ret = queue_init(&queue);
+//     assert(ret == 0 && "queue_init failed");
+
+//     assert(queue_is_empty(&queue) && "queue is not empty");
+
+//     return 0;
+// }
+
+
+// Create a new queue using the queue_create() function.
+// Add an item to the queue using the queue_enqueue() function.
+// Remove the item from the queue using the queue_dequeue() function.
+// Check if the queue is empty using the queue_empty() function.
+// The expected result is that the queue should be empty.
+// Test case to check if the first item added to the queue is the first one to be removed (FIFO behavior)
+// int main() {
+//     queue_t queue;
+//     int data = 42;
+//     int ret = queue_init(&queue);
+//     assert(ret == 0 && "queue_init failed");
+
+//     ret = queue_enqueue(&queue, &data);
+//     assert(ret == 0 && "queue_enqueue failed");
+
+//     assert(!queue_is_empty(&queue) && "queue is empty");
+
+//     return 0;
+// }
+
+
+// Create a new queue using the queue_create() function.
+// Add multiple items to the queue using the queue_enqueue() function.
+// Remove an item from the queue using the queue_dequeue() function.
+// Check if the removed item is the first item that was added to the queue.
+// Repeat the process for all items in the queue.
+// The expected result is that the removed items should match the order in which they were added.
+// Test case to check if the queue can handle a large number of items
+// int main() {
+//     queue_t queue;
+//     int data1 = 42;
+//     int data2 = 99;
+//     int ret = queue_init(&queue);
+//     assert(ret == 0 && "queue_init failed");
+
+//     ret = queue_enqueue(&queue, &data1);
+//     assert(ret == 0 && "queue_enqueue failed");
+
+//     ret = queue_enqueue(&queue, &data2);
+//     assert(ret == 0 && "queue_enqueue failed");
+
+//     int *ptr = (int*)queue_dequeue(&queue);
+//     assert(ptr != NULL && "queue_dequeue failed");
+//     assert(*ptr == 42 && "dequeued wrong element");
+
+//     return 0;
+// }
+
+
+// Create a new queue using the queue_create() function.
+// Add a large number of items to the queue using the queue_enqueue() function.
+// Check if the size of the queue matches the number of items added.
+// Remove all items from the queue using the queue_dequeue() function.
+// Check if the queue is empty using the queue_empty() function.
+// The expected result is that the size of the queue should match the number of items added and that the queue should be empty after removing all items.
+// int main() {
+//     queue_t queue;
+//     int ret = queue_init(&queue);
+//     assert(ret == 0 && "queue_init failed");
+
+//     void *ptr = queue_dequeue(&queue);
+//     assert(ptr == NULL && "queue_dequeue failed");
+
+//     return 0;
+// }
--- libuthread/queue.c
+++ libuthread/queue.c
@@ -9,15 +9,17 @@
 // Have an address in the queue_t
 // makefile
 
-struct node {
-	void * data;
-	struct node * next;
+struct node
+{
+	void *data;
+	struct node *next;
 };
 
-struct queue {
+struct queue
+{
 	/* TODO Phase 1 */
-	struct node * head;
-	struct node * tail;
+	struct node *head;
+	struct node *tail;
 	int length;
 };
 
@@ -27,7 +29,7 @@ queue_t queue_create(void)
 	queue_t queue = malloc(sizeof(struct queue));
 
 	// Check if malloc failed
-	if(queue == NULL)
+	if (queue == NULL)
 		return NULL;
 	// Else, initialize queue
 	queue->head = NULL;
@@ -39,18 +41,19 @@ queue_t queue_create(void)
 // Hayden O(1)
 int queue_destroy(queue_t queue)
 {
-	if (queue != NULL) {
-        return -1;
-    }
-    // struct node *current = queue->head;
-    // struct node *next;
-    // while (current != NULL) {
-    //     next = current->next;
-    //     free(current);
-    //     current = next;
-    // }
-    free(queue);
-    return 0;
+	if (queue != NULL)
+	{
+		return -1;
+	}
+	// struct node *current = queue->head;
+	// struct node *next;
+	// while (current != NULL) {
+	//     next = current->next;
+	//     free(current);
+	//     current = next;
+	// }
+	free(queue);
+	return 0;
 }
 
 // Dillon O(1)
@@ -59,18 +62,18 @@ int queue_enqueue(queue_t queue, void *data)
 	// Check for valid inputs
 	if (queue == NULL || data == NULL)
 		return -1;
-	
+
 	// Create new node
-	struct node * newNode = malloc(sizeof(struct node));
+	struct node *newNode = malloc(sizeof(struct node));
 	if (newNode == NULL)
 		return -1;
-	
+
 	// Initialize new node
 	newNode->data = data;
 	newNode->next = NULL;
-	
+
 	// Add new node to queue
-	if(queue->head == NULL)
+	if (queue->head == NULL)
 		queue->head = newNode;
 	queue->tail = newNode;
 	queue->length++;
@@ -84,19 +87,19 @@ int queue_dequeue(queue_t queue, void **data)
 	// Check for valid inputs
 	if (queue == NULL || data == NULL || queue->head == NULL || queue->tail == NULL || queue->length == 0)
 		return -1;
-	
+
 	// Set head data from queue to data
 	*data = queue->head->data;
 
 	// Remove head from queue and update the queue
-	struct node * temp = queue->head;
+	struct node *temp = queue->head;
 	queue->head = queue->head->next;
 	queue->length--;
 	free(temp);
 
 	if (queue->length == 0)
 		queue->tail = NULL;
-	
+
 	return 0;
 }
 
@@ -104,41 +107,62 @@ int queue_dequeue(queue_t queue, void **data)
 int queue_delete(queue_t queue, void *data)
 {
 	// Initialize pointers
-    struct node* temp = queue->head;
-    struct node* prev = NULL;
+	struct node *temp = queue->head;
+	struct node *prev = NULL;
 
-    // Traverse the linked list
-    while (temp != NULL && temp->data != data) {
-        prev = temp;
-        temp = temp->next;
-    }
+	// Traverse the linked list
+	while (temp != NULL && temp->data != data)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
 
-    // If the data is found, delete the node
-    if (temp != NULL) {
-        // If the node is the head of the linked list
-        if (prev == NULL) {
-            queue->head = temp->next;
-        }
-        // If the node is not the head of the linked list
-        else {
-            prev->next = temp->next;
-        }
-        // Free the memory used by the node
-        free(temp);
-        // Decrement the length of the linked list
-        queue->length--;
-        return 0;
-    }
-    // If the data is not found, return an error
-    else {
-        return -1;
-    }
+	// If the data is found, delete the node
+	if (temp != NULL)
+	{
+		// If the node is the head of the linked list
+		if (prev == NULL)
+		{
+			queue->head = temp->next;
+		}
+		// If the node is not the head of the linked list
+		else
+		{
+			prev->next = temp->next;
+		}
+		// Free the memory used by the node
+		free(temp);
+		// Decrement the length of the linked list
+		queue->length--;
+		return 0;
+	}
+	// If the data is not found, return an error
+	else
+	{
+		return -1;
+	}
 }
 
 // Together NOT O(1)
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* TODO Phase 1 */
+	if (queue == NULL || func == NULL)
+		return -1;
+
+	if(queue->length <= 0)
+		return 0;
+	
+	// Call func on each node in the queue
+	struct node *node;
+	struct node *next = queue->head; 
+	while (node != NULL || next != NULL)
+	{
+		node = next;
+		next = node->next;
+		func(queue, node->data);
+	}
+
+	return 0;
 }
 
 // Hayden O(1)
@@ -147,4 +171,3 @@ int queue_length(queue_t queue)
 	/* TODO Phase 1 */
 	return queue->length;
 }
-
