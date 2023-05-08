13d69abba16d2e6f8dd9bb8eed531ce48618ec31
DJ200148 <dillonjackson01@gmail.com>
2023-05-08 03:15:02 +0000

    Phase 4 started
    Co-authored-by: Hayden Balsys <haydenbalsys@users.noreply.github.com>

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,30 +15,59 @@
  */
 #define HZ 100
 
-static sigset_t x;
-static struct sigaction y;
-static struct itimerval z;
-
+sigset_t x;
+struct sigaction y;
+struct itimerval z;
 
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
-	//sigprocmask(SIG_BLOCK, &x, NULL);
+	sigemptyset(&x);
+	sigaddset(&x, SIGVTALRM);
+	sigprocmask(SIG_BLOCK, &x, NULL);
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+	/* Enable preemption by unblocking the SIGVTALRM signal */
+	sigemptyset(&x);
+	sigaddset(&x, SIGVTALRM);
+	sigprocmask(SIG_UNBLOCK, &x, NULL);
 }
 
 void preempt_start(bool preempt)
 {
 	/* TODO Phase 4 */
-	(void) preempt;
+	if (preempt) {
+        /* Block signal temporarily */
+        sigemptyset(&x);
+        sigaddset(&x, SIGVTALRM);
+        sigprocmask(SIG_BLOCK, &x, NULL);
+
+        /* Install signal handler */
+        y.sa_handler = &uthread_yield;
+        sigemptyset(&y.sa_mask);
+        y.sa_flags = 0;
+        sigaction(SIGVTALRM, &y, NULL);
+
+        /* Configure timer */
+        z.it_value.tv_sec = 0;
+        z.it_value.tv_usec = 1000000 / HZ;
+        z.it_interval.tv_sec = 0;
+        z.it_interval.tv_usec = 1000000 / HZ;
+        setitimer(ITIMER_VIRTUAL, &z, NULL);
+
+        /* Unblock signal */
+        sigprocmask(SIG_UNBLOCK, &x, NULL);
+    }
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+	//restore configuration
+	sigaction(SIGVTALRM, &old_action, NULL);
+	setitimer(ITIMER_VIRTUAL, &old_itimerval, NULL);
 }
 
